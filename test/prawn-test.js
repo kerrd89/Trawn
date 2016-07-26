b08afe07622ed557b4db507bf96ba92bdcefd44b
David Kerr <kerrd89@gmail.com>
2016-07-26 14:48:17 -0600

    move and move tests implemented

--- test/prawn-test.js
+++ test/prawn-test.js
@@ -16,9 +16,18 @@ describe('Prawn', function() {
             assert.equal(prawn.y, 5);
             // assert.equal(prawn.direction, 'left');
         });
+    });
+
+
+    context('with given attributes', function() {
+        // var xValue = 7;
+        // var yValue = 7;
+        // var direction = 'left';
+        // var options = {x: xValue, y: yValue, dir: direction}
 
         describe('moveRight', function() {
             it('should increment the x position by 1', function() {
+                var prawn = new Prawn({})
                 var xValue = prawn.x;
                 prawn.moveRight();
                 assert.equal(prawn.x, xValue + 1);
@@ -26,18 +35,40 @@ describe('Prawn', function() {
         });
 
         describe('moveLeft', function() {
-            it('should decrement the x position by 1', function() {});
+            it('should decrement the x position by 1', function() {
+                var prawn = new Prawn({})
+                var xValue = prawn.x;
+                prawn.moveLeft();
+                assert.equal(prawn.x, xValue - 1);
+            });
         });
 
         describe('moveUp', function() {
-            it('should decrement the y position by 1', function() {});
+            it('should decrement the y position by 1', function() {
+                var prawn = new Prawn({})
+                var yValue = prawn.y;
+                prawn.moveUp();
+                assert.equal(prawn.y, yValue - 1);
+            });
         });
 
         describe('moveDown', function() {
-            it('should increment the y position by 1', function() {});
+            it('should increment the y position by 1', function() {
+                var prawn = new Prawn({})
+                var yValue = prawn.y;
+                prawn.moveDown();
+                assert.equal(prawn.y, yValue + 1);
+            });
         });
-    // context('with given attributes', function() {
-    //   var xValue
 
+        describe('move', function() {
+          it('should change x or y depending on direction', function() {
+            debugger;
+            var prawn = new Prawn({});
+            var xValue = prawn.x;
+            prawn.move();
+            assert.equal(prawn.x, xValue - 1)
+          });
+        });
     });
 });
