6928f93c50f716bdd10b626a5e0eaa30a8f2432d
David Kerr <kerrd89@gmail.com>
2016-07-27 15:51:24 -0600

    prawn stuff

--- test/prawn-world-test.js
+++ test/prawn-world-test.js
@@ -5,18 +5,28 @@ const World = require('../lib/world');
 const Prawn = require('../lib/prawn');
 
 describe('Prawn-world relationship', function() {
-  it('prawns can recognize the world', function() {
-  var world = new World(200, 200);
-  var prawn = new Prawn({world: world});
-  assert.equal(prawn.world, world);
-  })
+    it('prawns can recognize the world', function() {
+        var world = new World(200, 200);
+        var prawn = new Prawn({
+            world: world
+        });
+        assert.equal(prawn.world, world);
+    })
 
-  it('prawns created with addPrawn should recognize the world', function() {
-    var world = new World(200, 200);
-    var prawn = new Prawn({});
-    world.addPrawn(prawn);
-    debugger;
-    assert.equal(prawn.world, world);
-
-  })
+    it('prawns created with addPrawn should recognize the world', function() {
+        var world = new World(200, 200);
+        var prawn = new Prawn({});
+        world.addPrawn(prawn);
+        assert.equal(prawn.world, world);
+        // assert.include(world.prawns, prawn);
+    })
 });
+
+describe('World in relation to Prawns', function() {
+    it('should know about the blocks added to the world', function() {
+        var world = new World(200, 200);
+        var prawn = new Prawn({});
+        world.addPrawn(prawn);
+        assert.include(world.prawns, prawn);
+    });
+})
